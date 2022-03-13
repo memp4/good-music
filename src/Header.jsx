91fbabf3a1a1f9bf728b27c5aa9a0928110fc89d
Sultan <godsultan42@gmail.com>
2022-03-13 23:46:49 +0100

    - Cleaned project - Draft layout - Layout header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Logo</a>
+            <a href="">Cart</a>
+        </div>
+    );
+};
+
+export default Header;
